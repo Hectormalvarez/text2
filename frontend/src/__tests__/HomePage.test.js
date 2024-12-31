ab01ef5f0d850991cfd97a38ed815ba541b92aeb
Hector Alvarez <alvarez.hectorm@outlook.com>
2024-12-31 10:51:07 -0600

    test(frontend): Improve test setup for HomePage
      Added `afterEach` hook with `cleanup` to ensure a clean state between tests.
      Removed unnecessary `beforeAll` block.
      Moved rendering and element retrieval within each `it` block for better readability and independence.

--- frontend/src/__tests__/HomePage.test.js
+++ frontend/src/__tests__/HomePage.test.js
@@ -1,20 +1,20 @@
 import '@testing-library/jest-dom'
-import { render, screen } from '@testing-library/react';
+import { render, screen, cleanup } from '@testing-library/react';
 import HomePage from 'src/app/page';
 
 describe('HomePage', () => {
-  let headingElement;
-
-  beforeEach(() => {
-    render(<HomePage />);
-    headingElement = screen.getByRole('heading', { level: 1 });
+  afterEach(() => {
+    cleanup();
   });
-
   it('renders the welcome message', () => {
+    render(<HomePage />);
+    const headingElement = screen.getByRole('heading', { level: 1 });
     expect(headingElement).toHaveTextContent('Welcome to TextShare!');
   });
 
   it('applies the correct CSS class to the heading', () => {
+    render(<HomePage />);
+    const headingElement = screen.getByRole('heading', { level: 1 });
     expect(headingElement).toHaveClass('title');
   });
 });
