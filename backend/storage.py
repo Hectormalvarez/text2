f3aa6d3f9d78cd2d09cd1dc27c5548d461b70b56
Hector Alvarez <alvarez.hectorm@outlook.com>
2025-01-25 11:16:17 -0600

    feat(backend): Implement InMemoryStorage and its tests

--- backend/storage.py
+++ backend/storage.py
@@ -1,6 +1,8 @@
 from abc import ABC, abstractmethod
 from models import Snippet
 
+import uuid
+
 class Storage(ABC):
     @abstractmethod
     def create_snippet(self, snippet: Snippet) -> str:
@@ -13,3 +15,23 @@ class Storage(ABC):
     @abstractmethod
     def delete_snippet(self, snippet_id: str) -> None:
         pass
+
+
+class InMemoryStorage(Storage):
+    def __init__(self):
+        self.snippets = {}
+    
+    def create_snippet(self, snippet: Snippet) -> str:
+        """Creates a new snippet and returns the ID"""
+        snippet_id = str(uuid.uuid4())[:6]
+        self.snippets[snippet_id] = snippet
+        return snippet_id
+    
+    def get_snippet(self, snippet_id: str) -> Snippet | None:
+        """Retreives a snippet by its ID."""
+        return self.snippets.get(snippet_id)
+    
+    def delete_snippet(self, snippet_id: str) -> None:
+        """Deletes a snippet by its ID."""
+        if snippet_id in self.snippets:
+            del self.snippets[snippet_id]
